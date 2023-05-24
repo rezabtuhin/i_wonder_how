78b5c4041756d93ccba4a245eb8132bb71ac20fe
rezabuddawla <rezabuddawlatuhin@gmail.com>
2023-05-24 12:41:40 +0600

    individual components

--- app/number/[id]/page.jsx
+++ app/number/[id]/page.jsx
@@ -1,32 +1,38 @@
 "use client";
+import Binary from "@components/Binary";
+import Decimal from "@components/Decimal";
+import Hexa from "@components/Hexa";
+import Octal from "@components/Octal";
 import { useParams } from "next/navigation";
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 
 const page = () => {
   const params = useParams();
   const uid = params.id;
   let title = "";
 
-    function isBinaryNumber(str) {
-        const pattern = /^[01]+(\.[01]+)?$/;
-        return pattern.test(str);
-    }
-
-
-    const [searchText, setSearchText] = useState("");
-  const [valid, setValid] = useState(true);
-  const handleSearchChange = (e) =>{
-      const value = e.target.value;
-      setSearchText(value);
-
+  function isBinaryNumber(str) {
+    const pattern = /^[01]+(\.[01]+)?$/;
+    return pattern.test(str);
   }
+
+  const [searchText, setSearchText] = useState("");
+  const [ binary, setBinary ] = useState("");
+  const [ decimal, setDecimal ] = useState("");
+  const [ octal, setOctal ] = useState("");
+  const [ hexa, setHexa ] = useState("");
+  const [valid, setValid] = useState(true);
+  const handleSearchChange = (e) => {
+    const value = e.target.value;
+    setSearchText(value);
+  };
   if (uid === "1") {
     title = "Binary to__";
     return (
       <section className="w-full flex-col">
         <h1 className="head_text_2 blue_gradient">{title}</h1>
         <form className="relative w-full flex-col">
-            <p className={"logo_text flex flex-end"}></p>
+          <p className={"logo_text flex flex-end"}></p>
           <input
             type={"text"}
             placeholder={"Enter your binary number"}
@@ -34,9 +40,9 @@ const page = () => {
             onChange={handleSearchChange}
             required
             className={`search_input peer`}
-            pattern="[01]+"
           />
         </form>
+        <Binary/>
       </section>
     );
   } else if (uid === "2") {
@@ -54,6 +60,7 @@ const page = () => {
             className={"search_input peer"}
           />
         </form>
+        <Octal/>
       </section>
     );
   } else if (uid === "3") {
@@ -71,6 +78,7 @@ const page = () => {
             className={"search_input peer"}
           />
         </form>
+        <Decimal/>
       </section>
     );
   } else if (uid === "4") {
@@ -88,6 +96,7 @@ const page = () => {
             className={"search_input peer"}
           />
         </form>
+        <Hexa/>
       </section>
     );
   } else {
---
+++ components/Decimal.jsx
@@ -0,0 +1,32 @@
+const Decimal = () => {
+  return (
+    <div className="flex flex-wrap -mx-4 my-5">
+      <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/3 px-4 mb-4">
+        <div className="max-w-sm rounded overflow-hidden shadow-lg bg-slate-200">
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">Binary</div>
+            <p className="text-gray-700 text-base"></p>
+          </div>
+        </div>
+      </div>
+      <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/3 px-4 mb-4">
+        <div className="max-w-sm rounded overflow-hidden shadow-lg bg-slate-200">
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">Octal</div>
+            <p className="text-gray-700 text-base"></p>
+          </div>
+        </div>
+      </div>
+      <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/3 px-4 mb-4">
+        <div className="max-w-sm rounded overflow-hidden shadow-lg bg-slate-200">
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">Hexa-Decimal</div>
+            <p className="text-gray-700 text-base"></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Decimal;
